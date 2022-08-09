d42dbc52d0faa83508287c11fefe87881d9ee44f
gips-tutor <78002461+gips-tutor@users.noreply.github.com>
2022-08-09 10:06:04 +0900

    Initial commit

---
+++ src/main/java/com/gips/ourapp/OurappApplication.java
@@ -0,0 +1,13 @@
+package com.gips.ourapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurappApplication.class, args);
+	}
+
+}
