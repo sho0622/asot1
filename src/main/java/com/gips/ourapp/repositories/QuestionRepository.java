27fda491ddf707cd10407b1a87557ebf4581c55d
suzuki-s <gips-suzuki-s@outlook.jp>
2022-08-17 13:34:04 +0900

    create packege

---
+++ src/main/java/com/gips/ourapp/repositories/QuestionRepository.java
@@ -0,0 +1,14 @@
+package com.gips.ourapp.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.gips.ourapp.entities.QuestionEntity;
+
+public interface QuestionRepository extends JpaRepository<QuestionEntity, String> {
+	// ランダムに１０件取得
+	@Query(value = "select * from m_question order by random() limit 2;", nativeQuery = true)
+	List<QuestionEntity> findQuestion();
+}
