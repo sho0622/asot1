5483a1b351c59c3f545b16f892d5ec38006e5086
suzuki-s <gips-suzuki-s@outlook.jp>
2022-08-25 10:31:53 +0900

    コンストラクタ　コメント

--- src/main/java/com/gips/ourapp/forms/AnswerForm.java
+++ src/main/java/com/gips/ourapp/forms/AnswerForm.java
@@ -15,17 +15,17 @@ public class AnswerForm {
 	private String answer8;
 	private String answer9;
 	private String answer10;
-
-	AnswerForm() {
-		this.answer[0] = "answer1";
-		this.answer[1] = "answer2";
-		this.answer[2] = "answer3";
-		this.answer[3] = "answer4";
-		this.answer[4] = "answer5";
-		this.answer[5] = "answer6";
-		this.answer[6] = "answer7";
-		this.answer[7] = "answer8";
-		this.answer[8] = "answer9";
-		this.answer[9] = "answer10";
-	}
+// コンストラクタ
+//	AnswerForm() {
+//		this.answer[0] = "answer1";
+//		this.answer[1] = "answer2";
+//		this.answer[2] = "answer3";
+//		this.answer[3] = "answer4";
+//		this.answer[4] = "answer5";
+//		this.answer[5] = "answer6";
+//		this.answer[6] = "answer7";
+//		this.answer[7] = "answer8";
+//		this.answer[8] = "answer9";
+//		this.answer[9] = "answer10";
+//	}
 }
